10768cd2bc6e3b17137e5a791f4917364959bd1e
betcorg <84089238+betcorg@users.noreply.github.com>
2024-12-23 09:13:23 -0600

    Update to version 0.0.3

--- examples/index.ts
+++ examples/index.ts
@@ -1,16 +1,14 @@
-// import fs from 'fs';
+import fs from 'fs';
 import { splitter, SplitOptions } from '../dist';
 
-// const text = fs.readFileSync('samples/lorem.txt', 'utf-8');
-const text =
-    'Item 1; Item 2; Item 3; Item 4';
+const text = fs.readFileSync('samples/article.md', 'utf-8');
 
 const options: SplitOptions = {
     // minLength: 0,
-    // maxLength: 10000,
-    // splitter: 'markdown',
+    maxLength: 1000,
+    splitter: 'markdown',
     // overlap: 5,
-    regex: /[.]/,
+    // regex: '',
     // removeExtraSpaces: true,
 };
 
@@ -22,7 +20,3 @@ for (const chunk of chunks) {
     console.log(chunk);
     console.log('\n*********************************\n');
 }
-
-
-// const text =  'Item 1. Item 2. Item 3. Item 4.';
-// console.log(text.split('. '));
