6871fb2240775aa3d9a166fae2806632b012bcdd
betcorg <84089238+betcorg@users.noreply.github.com>
2024-12-25 09:23:34 -0600

    Optimize tests. Reduce redundant code

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -8,12 +8,16 @@ describe('Text splitters', () => {
         'samples/artificial-intelligence.md',
         'utf-8'
     );
+    const fewSentences =
+        'This is the first sentence. This is the second, slightly longer sentence. And a final short one.';
 
-    const expectedOverlapedChunks = JSON.parse(fs.readFileSync(
-        'test/expec-overlap.json',
-        'utf-8',
-    ));
+    const {md_ovelap_50, txt_overlap_5_maxlength_30} = JSON.parse(
+        fs.readFileSync('test/expect-overlap.json', 'utf-8')
+    );
 
+    /**
+     * TESTS
+     */
     it('Text should be splitted into 795 sentences.', async () => {
         const sentences = splitter(text);
         expect(sentences.length).to.equal(795);
@@ -29,15 +33,28 @@ describe('Text splitters', () => {
         expect(paragraphs.length).to.equal(7);
     });
 
-    it('Markdown text should be splitted into 7 topics with arround 50 caracters of overlapping text', async () => {
-        const paragraphs = splitter(markdown, { splitter: 'markdown', overlap: 50 });
+    it('Markdown text should be splitted into 7 topics with an overlap of arround 50 caracters.', async () => {
+        const paragraphs = splitter(markdown, {
+            splitter: 'markdown',
+            overlap: 50,
+        });
         expect(paragraphs.length).to.equal(7);
-        expect(expectedOverlapedChunks[0]).to.equal(paragraphs[0]);
-        expect(expectedOverlapedChunks[1]).to.equal(paragraphs[1]);
-        expect(expectedOverlapedChunks[2]).to.equal(paragraphs[2]);
-        expect(expectedOverlapedChunks[3]).to.equal(paragraphs[3]);
-        expect(expectedOverlapedChunks[4]).to.equal(paragraphs[4]);
-        expect(expectedOverlapedChunks[5]).to.equal(paragraphs[5]);
-        expect(expectedOverlapedChunks[6]).to.equal(paragraphs[6]);
+
+        md_ovelap_50.forEach((chunk: string, i: number) => {
+            expect(chunk).to.equal(paragraphs[i]);
+        });
+
+    });
+
+    it('Short text should be splitted into 5 sentences of maximum 30 characters and an overlap of arround 5 characters.', async () => {
+        const sentences = splitter(fewSentences, {
+            splitter: 'sentence',
+            maxLength: 30,
+            overlap: 5,
+        });
+        expect(sentences.length).to.equal(5);
+        txt_overlap_5_maxlength_30.forEach((chunk: string, i: number) => {
+            expect(chunk).to.equal(sentences[i]);
+        });
     });
 });
