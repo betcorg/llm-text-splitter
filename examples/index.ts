4290f169a6fc0cd8f571b5829f787250cc96a606
betcorg <84089238+betcorg@users.noreply.github.com>
2024-12-28 08:21:21 -0600

    Add markdown example

--- examples/index.ts
+++ examples/index.ts
@@ -3,6 +3,7 @@ import { splitter, SplitOptions } from '../dist';
 
 (async () => {
     const text = await fs.readFile('samples/lorem-ipsum.txt', 'utf-8');
+    const markdown = await fs.readFile('samples/artificial-intelligence.md', 'utf-8');
 
     const createFile = async (filePath: string, chunks: string[]) => {
         let chunkText = '';
@@ -18,7 +19,7 @@ import { splitter, SplitOptions } from '../dist';
     /**
      * Default values.
      */
-    let options: SplitOptions = {
+    const options: SplitOptions = {
         minLength: 0,
         maxLength: 5000,
         splitter: 'sentence',
@@ -30,24 +31,22 @@ import { splitter, SplitOptions } from '../dist';
     /**
      * Split text into sentences.
      */
-    const sentences = splitter(text, options);
+    const sentences = splitter(text);
     await createFile('examples/sentences.txt', sentences);
 
     /**
      * Split text into paragraphs.
      */
-    options = {
-        splitter: 'paragraph',
-    };
+    options.splitter = 'paragraph';
     const paragraphs = splitter(text, options);
     await createFile('examples/paragraphs.txt', paragraphs);
 
     /**
-     * Split text into paragraphs.
+     * Split text into markdown headings.
      */
-    options = {
-        splitter: 'markdown',
-    };
-    const topics = splitter(text, options);
-    await createFile('examples/paragraphs.txt', topics);
+    options.splitter ='markdown';
+    const topics = splitter(markdown, options);
+    await createFile('examples/markdown-topics.txt', topics);
+
+    console.log('Results are ready at directory: examples/');
 })();
